20fe178d8e5b1262807621f6b074842a5290dba3
agomila96@gmail.com <aitor2008>
2020-12-17 16:06:28 +0100

    Use material design

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:xstore_mobile/src/views/routes/home.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
       title: "XStore Mobile",
