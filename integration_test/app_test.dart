a8e0cdf5807873a57668051474c978825fa22eaa
agomila96@gmail.com <aitor2008>
2020-12-17 16:06:33 +0100

    Fix tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:app/main.dart' as app;
+import 'package:xstore_mobile/main.dart' as app;
 
 void main() => run(_testMain);
 
