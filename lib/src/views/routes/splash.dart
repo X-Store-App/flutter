9326a5ca69f75305195becbaf2c7f1491c9c2f09
agomila96@gmail.com <aitor2008>
2020-12-17 19:01:59 +0100

    Add splashscreen

---
+++ lib/src/views/routes/splash.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:xstore_mobile/product/data.dart';
+
+class SplashScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Image.asset(icon),
+    ));
+  }
+}
